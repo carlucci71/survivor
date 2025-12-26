e2c8ed28923cd3490499ed5d867bbc03fcb1cf75
d.carlucci <carlucci.daniele@gmail.com>
2025-12-26 17:22:04 +0100

    tema

--- survivor_webapp/src/environments/environment.ts
+++ survivor_webapp/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8389/api/survivorBe',
   theme: {
-    primary: '#4CAF50',
-    name: 'green'
+    primary: '#6a4cafff',
+    name: 'indigo-pink'
   }
 };
