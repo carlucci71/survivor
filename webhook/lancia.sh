8f97882bc183ccc41d584b72c8a4ac8b09078c89
Daniele Carlucci <carlucci.daniele@gmail.com>
2025-12-28 16:29:28 +0100

    aaa

--- webhook/lancia.sh
+++ webhook/lancia.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-webhook -verbose -hooks hooks.json -port 9000 -template      
+source /home/survivor/.env
+envsubst < hooks.json.template > hooks.json
+webhook -verbose -hooks hooks.json -port 9000
