2dc454226d05efe110481e5813fbaff7b35f3f79
Daniele Carlucci <carlucci.daniele@gmail.com>
2026-01-25 11:38:28 +0100

    app android

---
+++ survivor_webapp/src/environments/environment.mobile.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  ambiente: 'DEV-MOBILE',
+  mobile: true,
+  apiUrl: 'http://10.0.2.2:8389/api/survivorBe',
+  baseUrl: 'http://localhost:4200',
+  theme: {
+    primary: '#6a4cafff',
+    name: 'indigo-pink'
+  }
+};
