f15626839abb091c133213b3ca7a4f93fbc2126f
Daniele Carlucci <carlucci.daniele@gmail.com>
2026-01-28 21:27:26 +0100

    invio notifica

--- survivor_backend/src/main/java/it/ddlsolution/survivor/scheduled/ScheduledPushNotifications.java
+++ survivor_backend/src/main/java/it/ddlsolution/survivor/scheduled/ScheduledPushNotifications.java
@@ -31,7 +31,7 @@ public class ScheduledPushNotifications {
      * Esegue ogni 15 minuti e invia notifiche per le partite che iniziano tra ~1 ora
      * (finestra: tra 50 e 70 minuti da ora)
      */
-    //@Scheduled(cron = "0 */1 * * * ?") // Ogni 15 minuti
+    @Scheduled(cron = "0 */1 * * * ?") // Ogni 1 minuti
     public void sendUpcomingMatchNotifications() {
         PushNotificationDTO notification = PushNotificationDTO.builder()
                 .title("Partita in arrivo!")
