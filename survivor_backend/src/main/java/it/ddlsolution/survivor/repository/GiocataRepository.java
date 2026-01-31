5a90f57b39b87eefb8439b3749f8f1d428958a90
Daniele Carlucci <carlucci.daniele@gmail.com>
2026-01-31 19:36:13 +0100

    fix

--- survivor_backend/src/main/java/it/ddlsolution/survivor/repository/GiocataRepository.java
+++ survivor_backend/src/main/java/it/ddlsolution/survivor/repository/GiocataRepository.java
@@ -11,10 +11,6 @@ public interface GiocataRepository extends JpaRepository<Giocata, Long> {
 
     Optional<Giocata> findByGiornataAndGiocatore_IdAndLega_Id(Integer giornata, Long idGiocatore, Long idLega);
 
-    /**
-     * Conta il numero di giocate per un giocatore in una lega
-     */
-    Long countByGiocatore_IdAndLega_Id(Long giocatoreId, Long legaId);
 
     /**
      * Trova l'ultima giocata di un giocatore in una lega (per giornata)
